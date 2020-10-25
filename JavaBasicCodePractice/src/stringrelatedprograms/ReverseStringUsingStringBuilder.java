201f50da6ab4a165110eed5b9e619ee9133167cb
akhileshkrroy <krroy.akhilesh@gmail.com>
2020-10-24 20:12:47 -0400

    String Reverse Using StringBuilder

---
+++ JavaBasicCodePractice/src/stringrelatedprograms/ReverseStringUsingStringBuilder.java
@@ -0,0 +1,14 @@
+package stringrelatedprograms;
+
+public class ReverseStringUsingStringBuilder {
+
+	public static void main(String[] args) {
+
+		String str="Akhilesh";
+		StringBuilder strBuild=new StringBuilder(str);
+		
+		System.out.println(strBuild.reverse());
+		
+	}
+
+}
